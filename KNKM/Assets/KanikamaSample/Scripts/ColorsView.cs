7220eae3e0bfafe12134c51075055b5f8b692adb
shivaduke28 <shivaduke28@gmail.com>
2021-11-06 18:14:54 +0900

    kanikama

---
+++ KNKM/Assets/KanikamaSample/Scripts/ColorsView.cs
@@ -0,0 +1,27 @@
+﻿using Kanikama.Udon;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kanikama.Sample
+{
+    public class ColorsView : UdonSharpBehaviour
+    {
+        [SerializeField] KanikamaColorSampler colorSampler;
+        [SerializeField] RawImage[] rawImages;
+
+        Color[] colors;
+
+        void Start()
+        {
+            colors = colorSampler.GetColors();
+        }
+        void LateUpdate()
+        {
+            for (var i = 0; i < colors.Length; i++)
+            {
+                rawImages[i].color = colors[i];
+            }
+        }
+    }
+}
